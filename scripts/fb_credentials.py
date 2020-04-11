54d26d7f7d7d4610ce2593335a9a80fea27dc60a
kvanbendegem <kurtv7348@gmail.com>
2020-04-11 16:10:09 -0400

    error handling in credentials file

--- scripts/fb_credentials.py
+++ scripts/fb_credentials.py
@@ -3,4 +3,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_credentials():
-    return os.getenv("FB_USERNAME"), os.getenv("FB_PASSWORD")
+    username = os.getenv("FB_USERNAME")
+    password = os.getenv("FB_PASSWORD")
+
+    if not username or not password:
+        raise Exception('Could find both FB_USERNAME and FB_PASSWORD variables in environment')
+
+    return username, password
