2e7d237b72ad6ec27779a8b37b40d43be95f344e
Dragos Rotaru <dragos@rotaru.co>
2020-04-14 02:46:36 -0400

    fb groups ids now cleaned; added 30sec time for 2FA

--- scripts/facebook-groups/id-validator/__init__.py
+++ scripts/facebook-groups/id-validator/__init__.py
@@ -7,17 +7,17 @@ from utils import fb_session
 
 def is_group_id(id, session):
     session.driver.get("https://www.facebook.com/groups/" + id)
-    return len(session.driver.find_elements_by_partial_link_text("Create New Account")) < 1
+    return len(session.driver.find_elements_by_partial_link_text("Visit Help Center")) < 1 and "This Content Isn't Available Right Now" not in session.driver.page_source
 
 def main(input_file_path, output_file_path):
     df = pd.read_csv(input_file_path, sep='\n', header=None)
     df['valid'] = None
-    session = fb_session.get_session(with_log_in=False)
+    session = fb_session.get_session(with_log_in=True)
     for idx, item in df.iterrows():
         is_valid = is_group_id(item[0], session)
         print(is_valid)
         df.iloc[idx]['valid'] = is_valid
-        time.sleep(2)
+        time.sleep(5)
     df.to_csv(output_file_path, index=False)
 
 if __name__ == '__main__':
--- scripts/facebook-groups/utils/fb_session.py
+++ scripts/facebook-groups/utils/fb_session.py
@@ -3,6 +3,7 @@ from utils import fb_credentials
 from requestium import Session
 from requestium import Session, Keys
 import os
+import time
 
 def get_session(with_log_in=True):
     session = Session(webdriver_path = 'chromedriver',
@@ -16,4 +17,5 @@ def get_session(with_log_in=True):
         session.driver.ensure_element_by_id("email").send_keys(username)
         session.driver.ensure_element_by_id("pass").send_keys(password)
         session.driver.ensure_element_by_id("u_0_b").click() # login button
+        time.sleep(30)
     return session
