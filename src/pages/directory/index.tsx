1a2ff842275734be45def7eef54c9c637b75e5ad
Dragos Rotaru <dragos@rotaru.co>
2020-04-20 11:58:21 -0400

    added all fields

--- src/pages/directory/index.tsx
+++ src/pages/directory/index.tsx
@@ -5,6 +5,21 @@ import "./style.css";
 
 const columns = [
   {
+    key: "_id",
+    name: "Mongo ID",
+    display: false,
+  },
+  {
+    key: "id",
+    name: "ID",
+    display: false,
+  },
+  {
+    key: "scrapeID",
+    name: "Scrape Batch ID",
+    display: false,
+  },
+  {
     key: "name",
     name: "Name",
   },
@@ -13,18 +28,45 @@ const columns = [
     name: "Public",
   },
   {
+    key: "foundedOn",
+    name: "Created",
+  },
+  {
     key: "description",
     name: "Description",
   },
   {
+    key: "locations",
+    name: "Locations",
+  },
+  {
     key: "memberCount",
     name: "Members",
+  },
+  {
+    key: "memberCountIncreaseWeekly",
+    name: "Weekly Growth",
+  },
+  {
+    key: "postCountIncreaseDaily",
+    name: "Daily Posts",
+  },
+  {
+    key: "postCountIncreaseMonthly",
+    name: "Monthly Posts",
+  },
+  {
+    key: "scrapedAt",
+    name: "Last Updated",
   },
 ];
 
 export const Directory = () => (
   <div>
     <h1>Directory</h1>
-    <Table columns={columns} data={data} />
+    <Table
+      columns={columns.filter((col) => col.display !== false)}
+      data={data}
+    />
   </div>
 );
