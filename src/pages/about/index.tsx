0936b140034f695ce7bb9d6adb6e04688e245af8
Dragos Rotaru <dragos@rotaru.co>
2020-04-20 21:38:35 -0400

    wip

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,20 +4,49 @@ import "./style.css";
 export const About = () => (
   <div className="about-container">
     <div>
-      <h1>About</h1>
+      <h1>PPEForFree</h1>
+      <hr />
       <p>
-        Promoting and coordinating not-for-profit and nonprofit PPE initiatives
-        around the world.
+        In the age of information warfare, fake news and a polluted information
+        ecology, our capacity to engage in mutually beneficial decision-making
+        begins and ends with our ability to uncover Truth in the sea of noise.
+        The collective struggle to make sense of what is True and what is not
+        leads us to fundamental disagreements about the nature of reality - a
+        fragmentation of our understanding of what is and what ought to be.
       </p>
-      <iframe
-        title="Smarter Every Day Video"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/sbEj7M3aZIg?start=103"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <p>
+        Truth is not a matter of opinion; it is a common good, as much yours as
+        it is mine. Truth is impartial and apolitical. Truth is the fuel that
+        feeds the fires of Justice.
+      </p>
+      <p>
+        Every good decision is conceived by the seed of Truth. It sprouts when a
+        person acknowledges the Truth and it blooms when a person makes the
+        conscious choice to act on it. Our inability to make good decisions
+        perpetuates Human suffering. This is particularly evident in moments of
+        crisis, when the stakes are higher, the senses are hightened and the
+        pretense of normalcy is stripped away to expose the consequences of a
+        broken system laid bare. The Truth is, this pandemic doesn't care if you
+        did everything right. Our local actions have global consequences. We
+        need everyone, everywhere at every level of society to make better
+        decisions.
+      </p>
+      <p>Finding the Truth starts with quality information.</p>
+      <p>
+        The mission of this project is to grow a sense-making machine for the
+        people and by the people. A machine that combines the OSINT (Open Source
+        Intelligence) capabilities of the NSA, the ethos or community of
+        Wikipedia, the freedom of the internet and the determination of millions
+        of restless minds searching for the means to create a better tomorrow.
+      </p>
+      <p>
+        This machine will be used to break down information silos. It will be
+        used to provide tools that empower the individual. It will be used to
+        unify fragmented groups. It will be used to find truthful, accurate and
+        relevant data, and put it in the hands of those that wield the power to
+        use it for good. It will be used to amplify the signal and reduce the
+        noise. If this sounds vague, keep reading.
+      </p>
       <p>
         For a viable DIY PPE ecosystem to exist, we need coordination and
         cooperation. People need to fall in line with already existing efforts.
@@ -34,7 +63,14 @@ export const About = () => (
         standardize. We integrate. We unify. We increase the signal, reduce the
         noise.
       </p>
-      <p>See this video:</p>
+      <p>
+        Our first goal is to dramatically scale and boost the efficiency of the
+        grassroots decentralized Personal Protective Equipment supply chain that
+        has sprung up before our eyes. If you are not familiar, millions of
+        people and small businesses around the world are producing face masks,
+        face shields, ventilators and a whole range of supplies for their local
+        hospitals and other front line organizations.
+      </p>
 
       <h2>Lets Work Together</h2>
       <p>
