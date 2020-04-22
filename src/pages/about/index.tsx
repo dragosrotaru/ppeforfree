36a87583ed13745cd4d0c1b91afa405e4382db6d
Dragos Rotaru <dragos@rotaru.co>
2020-04-22 12:26:13 -0400

    manifesto draft 4

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,67 +7,34 @@ export const About = () => (
       <h1>PPEForFree</h1>
       <hr />
       <p>
-        In the age of information warfare, fake news and a polluted information
-        ecology, our capacity to engage in mutually beneficial decision-making
-        begins and ends with our ability to find capital-T universal Truth in
-        the sea of noise. The collective struggle to make sense of what is and
-        is not True leads us to fundamental disagreements about the nature of
-        reality - a fragmentation of our understanding of what is, what was and
-        what ought to be.
+        PPEForFree is a sense-making movement by the people and for the people.
+        We combine Open Source Intelligence (OSINT) and Data Science
+        capabilities with the ethos and community of Wikipedia to help light a
+        path for the millions of restless minds searching for the means to fight
+        the PPE shortage.
       </p>
       <p>
-        <strong>
-          Truth is not a matter of opinion; it is a common good, as much yours
-          as it is mine. Truth is impartial and apolitical. Truth is the fuel
-          that feeds the fires of Justice.
-        </strong>
+        Today we live in an age defined by an unprecedented amount of digital
+        information. Sadly, our information ecology, the internet, is polluted
+        with misinformation, low quality data, fake news, spam and a whole suite
+        of other related issues.
       </p>
       <p>
-        Every good decision is conceived by the seed of Truth. It sprouts when a
-        person acknowledges the Truth and it blooms when that person makes the
-        conscious choice to act on it. Our inability to make good decisions
-        perpetuates human suffering. This is particularly evident in moments of
-        crisis, when the stakes are higher, the senses are hightened and the
-        pretense of normalcy is stripped away to expose the failings of a broken
-        system laid bare.
+        Not only that, but we have a fragmented community spread out over
+        thousands of messaging boards, facebook groups, private messaging
+        channels and so forth. Some important innovation that happens in one
+        corner of the web doesn’t necessarily reach everywhere else. So we have
+        thousands of people reinventing the wheel.
       </p>
       <p>
-        The mission of this project is to grow a sense-making movement for the
-        people and by the people. A movement that combines the OSINT (Open
-        Source Intelligence) capabilities of the NSA, the ethos or community of
-        Wikipedia, the freedom of the internet and the determination of millions
-        of restless minds searching for the means to create a better tomorrow.
-      </p>
-      <p>
-        The solution is not simple or singular, It cannot be boiled down without
-        doing you a disservice and a detailed explanation is both beyond the
-        scope of this document and a lame way to convince you. Instead we will
-        demonstrate by example. Words are cheap. Many people have a lot of
-        things to say about world affairs but few take action. Furthermore, this
-        is all about thinking globally and acting locally. We are not
-        architecting a way to restructure civilization or anything crazy like
-        that. We are, at the core, tool makers and truth seekers looking to make
-        a dent and move the needle of progress.
-      </p>
-      <p>
-        So what is this? At minimum it is an attempt to fix the information
-        ecology of the PPE supply chain using a fresh new approach. Hence why
-        this site is called PPEForFree.
-      </p>
-      <p>
-        <strong>Ok enough talk... So where do we start?</strong>
-      </p>
-      <p>
-        Well right now there are regular people all over the world making PPE in
-        their community through what we call the Grassroots PPE Supply Chain. We
-        want to turn it up all the way to 11. We are going to amplify the
-        signal, reduce the noise.
-      </p>
-      <p>
-        <strong>
-          Step 1: make this site the central hub for Grassroots PPE Supply Chain
-          information.
-        </strong>
+        Our first goal is to dramatically scale and boost the efficiency of the
+        decentralized Personal Protective Equipment (PPE) supply chains that
+        have sprung up. Right now, there are regular people all over the world
+        making PPE in their community through what we call the Grassroots PPE
+        Supply Chain. We want to turn it up all the way to 11. We are going to
+        amplify the signal, reduce the noise by attempting to fix the
+        information ecology of the PPE supply chain using a fresh new approach.
+        How are we going to do this?
       </p>
       <ul>
         <li>
