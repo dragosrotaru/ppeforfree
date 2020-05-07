a1e35cfa034f4bd9bed735ef8cba7bd1e72ff9d0
Michael <michael@epsomsoftware.co.uk>
2020-05-07 17:26:50 +0100

    Refactor the facebook scraper into its own named files so that there is space for other scrapers

---
+++ scripts/src/models/primitive.ts
@@ -0,0 +1,7 @@
+export type UUID = string;
+export type FacebookID = string;
+export type TimeStamp = string;
+export type Url = string;
+export type WholeNumber = number;
+export type PhoneNumber = string;
+export type Email = string;
