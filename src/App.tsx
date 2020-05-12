1af40fc8c9f97a5469c417b39cc25bdef8149667
Domenic <marchesedb@gmail.com>
2020-05-11 20:39:24 -0400

    add mobile functionality for top menu, remove references to graph

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ import { About } from "./pages/about";
 import { Ideas } from "./pages/ideas";
 import { InitiativeDirectory } from "./pages/initiatives";
 import { InitiativeSubmission } from "./pages/initiative-submission";
-import { Graph } from "./pages/graph";
 
 function App() {
   return (
@@ -31,9 +30,47 @@ function App() {
               <TopAppBarTitle>#ppeforfree</TopAppBarTitle>
             </TopAppBarSection>
             <TopAppBarSection alignEnd>
-              <nav>
+              <div className="hamburger --align-end">
+                <button onClick={expandMenu} className="icon">
+                  <img src="/Hamburger_icon.png" alt="expand menu" className="icon-hamburger" />
+                </button>
+              </div>
+              <div id="sidebar" className="sidebar">
+                <button onClick={closeMenu} className="icon">
+                  <img src="/x_icon.png" alt="close menu" className="icon-hamburger" />
+                </button>
+                <div className="link-list">
+                  <Link to="/" className="link">directory</Link><br />
+                  <Link to="/about" className="link">about</Link><br />
+                  <Link to="/ideas" className="link">submit an idea</Link><br />
+                  <a
+                    href="https://discord.gg/pWF2zBf"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="link"
+                  >
+                    join us
+                  </a><br />
+                  <a
+                    href="https://twitter.com/ppeforfree"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="icon icon-twitter"
+                  >
+                    <img src="/twitter-icon.png" alt="twitter" className="icon-mobile" />
+                  </a><br />
+                  <a
+                    href="http://github.com/PPEForFree"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="icon icon-github"
+                  >
+                    <img src="/github-icon.png" alt="github" className="icon-mobile" />
+                  </a><br />
+                </div>
+              </div>
+              <nav className="nav-links">
                 <Link to="/">directory</Link>
-                <Link to="/graph">graph</Link>
                 <Link to="/about">about</Link>
                 <Link to="/ideas">submit an idea</Link>
                 <a
@@ -67,7 +104,6 @@ function App() {
 
         <Switch>
           <Route path="/ideas" component={Ideas} />
-          <Route path="/graph" component={Graph} />
           <Route path="/initiatives/submit" component={InitiativeSubmission} />
           <Route path="/about" component={About} />
           <Route path="/" component={InitiativeDirectory} />
@@ -75,6 +111,19 @@ function App() {
       </Router>
     </div>
   );
+}
+
+function expandMenu(){
+  const sidebar = document.getElementById("sidebar")
+  if(sidebar){
+    sidebar.style.width = "250px";
+  }
+}
+function closeMenu(){
+  const sidebar = document.getElementById("sidebar")
+  if(sidebar){
+    sidebar.style.width = "0";
+  }
 }
 
 export default App;
